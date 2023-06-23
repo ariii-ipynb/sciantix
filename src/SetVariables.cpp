06104b6e850f15de61e5d4f8ce0826aaf8d944cd
Giovanni <giovanni.nicodemo@mail.polimi.it>
2023-06-23 18:06:04 +0200

    new try with suppressed output for TD and a

--- src/SetVariables.cpp
+++ src/SetVariables.cpp
@@ -972,7 +972,7 @@ void SetVariables(int Sciantix_options[], double Sciantix_history[], double Scia
 	sciantix_variable[sv_counter].setInitialValue(Sciantix_variables[74]);
 	sciantix_variable[sv_counter].setFinalValue(Sciantix_variables[74]);
 	sciantix_variable[sv_counter].setOutput(toOutputChromiumContent);
-	++sv_counter;
+	++sv_counter; // 
 
 	sciantix_variable.emplace_back();
 	sciantix_variable[sv_counter].setName("Chromium solution");
